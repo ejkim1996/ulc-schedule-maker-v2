5cddc015676a9ce0f570367e158080710cdbdcc2
Laksh Chakraborty <lakshc2010@gmail.com>
2022-10-15 18:29:39 -0400

    config: added react with create-react-app, configured it to proxy to port 3001 for dev, and added a changed dev command

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import express, { Express, Request, Response } from 'express';
+import path from 'path';
 
 const app: Express = express();
-const port = 3000;
+const port = 3001;
 
-app.get('/', (_: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/', (_, res: Response) => {
+  res.send("test");
+});
+
+app.get('/api/test', (_, res: Response) => {
+  const test = { 
+    "data": "this is some data"
+  }
+
+  res.json(test);
+});
+
+app.get('*', (_, res: Response) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
 app.listen(port, () => {
