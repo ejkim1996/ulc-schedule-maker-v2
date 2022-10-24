4e3a0828bb20d7e2b29b9d9eb086bcbbf883da6e
Laksh Chakraborty <lakshc2010@gmail.com>
2022-10-23 22:11:29 -0400

    feature: added tailwind, improved login style

--- client_next/pages/login.tsx
+++ client_next/pages/login.tsx
@@ -1,10 +1,19 @@
-import type { NextPage } from 'next';
-import Link from 'next/link';
+import type { NextPage } from "next";
+import Link from "next/link";
 
 const Login: NextPage = () => {
     return (
-        <Link href={"/api/auth/google"}>Login through google.</Link>
-    )
+        <div className="grid place-items-center h-full">
+            <div className="flex flex-col gap-8">
+                <h1 className="text-4xl text-center">ULC Schedule Maker</h1>
+                <Link href="/api/auth/google">
+                    <a className="btn btn-primary self-center">
+                        Login with Google
+                    </a>
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
